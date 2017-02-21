7a293b35d437598f00e23a1a2479ea1bf453d6f5
mmitti <dev@mail.mmitti.info>
2017-02-21 17:30:28 +0900

    initial

---
+++ main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strconv"
+)
+
+func main() {
+	if len(os.Args) == 4 {
+		i, _ := strconv.Atoi(os.Args[1])
+		j := os.Args[2]
+		k, _ := strconv.Atoi(os.Args[3])
+
+		switch j {
+		case "+":
+			fmt.Print(i + k)
+			return
+		case "-":
+			fmt.Print(i - k)
+			return
+		}
+	}
+	fmt.Print("使い方:<a> <+/-> <b>")
+}
